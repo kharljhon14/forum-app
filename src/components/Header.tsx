10d7dbd065817372cfe32f25cec954bd5911dc43
kharle14 <enriquezkharl14@gmail.com>
2024-05-29 22:42:10 +0800

    [Feat] Header
    - Popover sign out

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,10 +6,15 @@ import {
   Navbar,
   NavbarBrand,
   NavbarContent,
-  NavbarItem
+  NavbarItem,
+  Popover,
+  PopoverContent,
+  PopoverTrigger
 } from '@nextui-org/react';
 import Link from 'next/link';
 import { ReactNode } from 'react';
+
+import * as actions from '@/actions';
 
 export default async function Header() {
   const session = await auth();
@@ -17,31 +22,46 @@ export default async function Header() {
 
   if (session?.user)
     authContent = (
-      <Avatar
-        src={session.user.image || ''}
-        alt="User"
-      />
+      <Popover placement="bottom">
+        <PopoverTrigger>
+          <Avatar
+            src={session.user.image || ''}
+            alt="User"
+          />
+        </PopoverTrigger>
+        <PopoverContent>
+          <div className="p-4">
+            <form action={actions.signOut}>
+              <Button type="submit">Sign Out</Button>
+            </form>
+          </div>
+        </PopoverContent>
+      </Popover>
     );
   else
     authContent = (
       <>
         <NavbarItem>
-          <Button
-            type="submit"
-            color="secondary"
-            variant="bordered"
-          >
-            Sign In
-          </Button>
+          <form action={actions.signIn}>
+            <Button
+              type="submit"
+              color="secondary"
+              variant="bordered"
+            >
+              Sign In
+            </Button>
+          </form>
         </NavbarItem>
         <NavbarItem>
-          <Button
-            type="submit"
-            color="primary"
-            variant="flat"
-          >
-            Sign Up
-          </Button>
+          <form action={actions.signIn}>
+            <Button
+              type="submit"
+              color="primary"
+              variant="flat"
+            >
+              Sign Up
+            </Button>
+          </form>
         </NavbarItem>
       </>
     );
