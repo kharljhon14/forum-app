d4aba81425136db39f30292d995fa7c5f7b0dbcc
kharle14 <enriquezkharl14@gmail.com>
2024-05-31 00:30:39 +0800

    [Feat] Create topic form form state

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,5 +1,6 @@
 'use server';
 
+import CreateTopicForm from '@/components/topics/CreateTopicForm';
 import { z } from 'zod';
 
 const createTopicSchema = z.object({
@@ -10,15 +11,29 @@ const createTopicSchema = z.object({
   description: z.string().min(10)
 });
 
-export async function createTopic(formData: FormData) {
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+  };
+}
+
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
   const result = createTopicSchema.safeParse({
     name: formData.get('name'),
     description: formData.get('description')
   });
 
   if (!result.success) {
-    console.log(result.error.flatten().fieldErrors);
+    return {
+      errors: result.error.flatten().fieldErrors
+    };
   }
 
   // Todo: Revalidate the home page
+
+  return { errors: {} };
 }
--- src/components/topics/CreateTopicForm.tsx
+++ src/components/topics/CreateTopicForm.tsx
@@ -1,3 +1,4 @@
+'use client';
 import {
   Button,
   Input,
@@ -6,16 +7,19 @@ import {
   PopoverTrigger,
   Textarea
 } from '@nextui-org/react';
+import { useFormState } from 'react-dom';
 import * as actions from '@/actions';
 
 export default function CreateTopicForm() {
+  const [formState, action] = useFormState(actions.createTopic, { errors: {} });
+
   return (
     <Popover placement="left">
       <PopoverTrigger>
         <Button color="primary">Create a Topic</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form action={actions.createTopic}>
+        <form action={action}>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-lg">Create a Topic</h3>
             <Input
